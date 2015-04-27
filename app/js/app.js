8a69f21585580c2225e604e8439975f346900160
Mac Heller-Ogden <mac@machellerogden.com>
2015-04-27 10:07:37 -0500

    adding basics

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+(function () {
+
+    var app = angular.module('aseo', []);
+
+    app.directive('aseoDynamicContent', function () {
+        return {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'app/js/templates/dynamic-content.html'
+        };
+    });
+
+
+}());
